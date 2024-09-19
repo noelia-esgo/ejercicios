564448bc8e619e577dce590242667b096732f491
noelia-esgo <noeneg88@gmail.com>
2024-09-19 21:58:40 +0200

    ejercicio 1 y 3

---
+++ BasicAlgorithms/app.js
@@ -0,0 +1,28 @@
+//!EJERCICIO 1
+
+let myFavorite= "Hulk";
+let x= 50;
+let h= 5
+let y= 10;
+let z= h+y;
+
+
+//!EJERCICIO 3
+
+console.log(10*5);
+
+console.log(10/2);
+
+console.log(15%9);
+
+let p= 10;
+let j=5;
+let o= p+j;
+console.log(o);
+
+let c= 10;
+let m= 5;
+let i= c*m;
+console.log(i);
+
+
