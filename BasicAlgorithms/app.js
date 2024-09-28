5c6a2173e82783cac5540201a3959b8f094c6756
noelia-esgo <noeneg88@gmail.com>
2024-09-28 08:28:20 +0200

    ejercicios basic algorithms completos

--- BasicAlgorithms/app.js
+++ BasicAlgorithms/app.js
@@ -95,4 +95,97 @@ const rickandMorty= rickAndMortyCharacters3.splice(1,1);
 
 console.log(rickAndMortyCharacters3);
 
+//!EJERCICIO 5
 
+const number1 = 10;
+const number2 = 20;
+const number3 = 2;
+
+
+if (number2/number1 == 2) {
+console.log("number2 dividido entre number1 es igual a 2");
+};
+
+if (number1 !== number2) {
+console.log("number1 es estrictamente distinto a number2");
+};
+
+if (number3 != number1) {
+console.log("number3 es distinto number1");
+};
+
+if (number3 * 5 == number1) {
+console.log("number3 por 5 es igual a number1");
+};
+
+if (number3 * 5 == number1 && number1 * 2 == number2) {
+console.log("number3 por 5 es igual a number1 Y number1 por 2 es igual a number2");
+};
+
+if (number2/2==number1 || number1/5==number3) {
+console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
+};
+
+
+//! EJERCICIO 6
+
+// 1.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
+for (let i = 0; i <= 9; i++) {
+    console.log(i);
+}
+
+// 1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo 
+// cuando el resto del numero dividido entre 2 sea 0.
+
+let numeros= [0,1,2,3,4,5,6,7,8,9]
+
+for (let i = 0; i < numeros.length; i++) 
+    if (numeros[i] % 2 === 0){
+        console.log(numeros[i]);
+    }
+
+
+// 1.3 Crea un bucle para conseguir dormir contando ovejas. 
+// Este bucle tiene que dar 10 vueltas, es decir, 10 console.log.
+// Muestra por consola un mensaje diciendo 'Intentando dormir 🐑' en cada vuelta del bucle 
+// y cambia el mensaje en la décima vuelta a 'Dormido!'
+
+let number= [1,2,3,4,5,6,7,8,9,10]
+
+for (let i = 0; i < number.length; i++){
+if(number[i]===1){
+    console.log('Intentando dormir 🐑')
+} 
+else if (number[i]===2){
+    console.log('Intentando dormir 🐑')
+}else if (number[i]===3){
+    console.log('Intentando dormir 🐑') }
+    else if (number[i]===4){
+        console.log('Intentando dormir 🐑')}
+        else if (number[i]===5){
+            console.log('Intentando dormir 🐑')}
+            else if (number[i]===6){
+                console.log('Intentando dormir 🐑')}
+                else if (number[i]===7){
+                    console.log('Intentando dormir 🐑')}
+                    else if (number[i]===8){
+                        console.log('Intentando dormir 🐑')}
+                        else if (number[i]===9){
+                            console.log('Intentando dormir 🐑')}
+                            else if(number[i]===10)
+                                console.log('Dormido!!!😪')
+                            }
+
+//otra forma
+
+let numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let e = 0; e < numbers.length; e++) {
+    if (numbers[e] === 10) {
+        console.log(e, 'Dormido!!!😪');
+    } else {
+        console.log(e,'Intentando dormir 🐑');
+    }
+}
+                            
+                            
